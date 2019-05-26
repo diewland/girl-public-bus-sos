20a4021e004709896d07a835d089de2d0d8598e9
diewland <diewland@gmail.com>
2019-05-26 20:01:17 +0700

    add sounds

--- app/src/main/java/com/diewland/girlpublicbussos/MainActivity.kt
+++ app/src/main/java/com/diewland/girlpublicbussos/MainActivity.kt
@@ -1,19 +1,36 @@
 package com.diewland.girlpublicbussos
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var media1:MediaPlayer? = null;
+    private var media2:MediaPlayer? = null;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // initialize medias
+        media1 = MediaPlayer.create(this, R.raw.aggressive1)
+        media2 = MediaPlayer.create(this, R.raw.aggressive2)
+
         val btnSos = findViewById<Button>(R.id.btn_sos)
+
+        // on click
         btnSos.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Some message", Toast.LENGTH_SHORT).show()
+            media1?.seekTo(1000)
+            media1?.start()
+        }
+
+        // on long click
+        btnSos.setOnLongClickListener{
+            media2?.seekTo(500)
+            media2?.start()
+            true
         }
 
     }
